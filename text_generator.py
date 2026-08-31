be4cd8576d42c817a588feb3583bea30bb225b05
kaviya-suresh-hash <Kaviyasureshj@gmail.com>
2026-08-31 09:11:36 +0530

    Update text generator

--- text_generator.py
+++ text_generator.py
@@ -1,13 +1,23 @@
 from transformers import pipeline
 
-pipe = pipeline("text-generation", model="EleutherAI/gpt-neo-125m")
+pipe = pipeline(
+    "text-generation",
+    model="EleutherAI/gpt-neo-125m"
+)
 
-prompt = "machine learning"
+prompt = "Artificial Intelligence is"
 
-result = pipe(prompt, max_new_tokens=100)
+result = pipe(
+    prompt,
+    max_new_tokens=100,
+    do_sample=True,
+    temperature=0.8,
+    top_p=0.9
+)
 
 generated_text = result[0]["generated_text"]
 
 print(generated_text)
+
 with open("generated_text.txt", "w", encoding="utf-8") as file:
     file.write(generated_text)
